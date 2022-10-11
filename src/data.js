e4ee2c4cf57637eb5f81b6f3fb6c48380c1b47ba
Maycon Jardim <mayconsmill@gmail.com>
2022-10-11 12:32:59 -0300

    Mock data e fake auth

---
+++ src/data.js
@@ -0,0 +1,27 @@
+module.exports = function () {
+    return {
+        products: [
+            { id: 1, name: "Kayak", category: "Aventura",
+                description: "Um barco para uma pessoa", price: 275.99 },
+            { id: 2, name: "Colete Salva-Vidas", category: "Aventura",
+                description: "Equipamento auxiliar de flutuação", price: 48.99 },
+            { id: 3, name: "Bola de Futebol Campo", category: "Futebol",
+                description: "Copa do Mundo 2022 Al Rihla", price: 129.99 },
+            { id: 4, name: "Corner Flags", category: "Futebol",
+                description: "Bandeirinha Penalty Para Árbitro",
+                price: 34.95 },
+            { id: 5, name: "Bomba para encher Bolas", category: "Futebol",
+                description: "Bomba De Ar MB Manual é ideal para encher suas bolas", price: 29.99 },
+            { id: 6, name: "Tênis Nike Revolution 6", category: "Tênis",
+                description: "Para caminhadas e corridas leves", price: 199.99 },
+            { id: 7, name: "Tênis Adidas Coreracer Masculino", category: "Tênis",
+                description: "Para caminhadas e corridas leves",
+                price: 290.95 },
+            { id: 8, name: "Tênis Cano Alto Redley Originals", category: "Tênis",
+                description: "Para o dia a dia ou para curtir os momentos de lazer", price: 125,99 },
+            { id: 9, name: "Camisa Vasco III 21/22", category: "camisas",
+                description: "Terceiro uniforme da temporada 21/22", price: 132.99 }
+        ],
+        orders: []
+    }
+  }
