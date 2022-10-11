6141cda6a37a68488bc9b609605284636a561ea7
Maycon Jardim <mayconsmill@gmail.com>
2022-10-11 14:15:10 -0300

    Product model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<div class="bg-success p-2 text-center text-white">
+  MayconStore
+  </div>
+  `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
