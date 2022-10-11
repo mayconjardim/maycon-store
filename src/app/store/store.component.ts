5eee99b2e99391f39def5215d22376763e98b4c8
Maycon Jardim <mayconsmill@gmail.com>
2022-10-11 16:23:41 -0300

    Paginacão

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -10,7 +10,9 @@ import { ProductRepository } from '../model/product.repository';
 export class StoreComponent implements OnInit {
 
   selectedCategory: string | undefined;
-  
+  productsPerPage = 4;
+  selectedPage = 1;
+
   constructor(private repository: ProductRepository) {  }
 
   ngOnInit(): void {
@@ -18,7 +20,9 @@ export class StoreComponent implements OnInit {
 
 
   get products(): Product[] {
-    return this.repository.getProducts(this.selectedCategory);
+    let pageIndex = (this.selectedPage - 1) * this.productsPerPage
+    return this.repository.getProducts(this.selectedCategory)
+    .slice(pageIndex, pageIndex + this.productsPerPage);
   }
 
   get categories(): string[] {
@@ -29,5 +33,25 @@ export class StoreComponent implements OnInit {
     this.selectedCategory = newCategory;
   }
 
+  changePage(newPage: number) {
+    this.selectedPage = newPage;
+  }
+
+  changePageSize(newSize: number) {
+    this.productsPerPage = Number(newSize);
+    this.changePage(1);
+  }
+
+  
+//   get pageNumbers(): number[] {
+//     return Array(Math.ceil(this.repository
+//     .getProducts(this.selectedCategory).length / this.productsPerPage))
+//  .  fill(0).map((x, i) => i + 1);
+//   }
+
+    get pageCount(): number {
+        return Math.ceil(this.repository
+            .getProducts(this.selectedCategory).length / this.productsPerPage)
+    }
 
 }
